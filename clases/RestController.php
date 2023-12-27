b067aa3b7c27ed8b34700d1cfcbcdaf6fd8ef0c0
crisandreduran <150270388+crisandreduran@users.noreply.github.com>
2023-12-27 12:50:00 -0300

    Update RestController.php

--- clases/RestController.php
+++ clases/RestController.php
@@ -1,17 +1,1 @@
-<?php
 
-$routesArray = explode("/",$_SERVER['REQUEST_URI']);
-$routesArray = array_filter($routesArray);
-
-/*===========================================================
- Cuando no viene ninguna petición
- ===========================================================*/
-if(count($routesArray)==0){
-
-    $json['estado']="404";
-    $json['resultado']="No encontrado.";
-
-    echo json_encode($json, http_response_code($json['estado']));
-
-    return;
-}
